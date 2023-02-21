4bfb04a53c650f73a347679d87b36e187226453f
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-02-21 00:51:33 -0300

    criado 4 caixas de input

--- src/components/Basal/Basal.jsx
+++ src/components/Basal/Basal.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import "./Basal.scss"
+import "./Basal.scss";
 
 const Basal = () => {
   return (
-    <section className='basal'>
+    <section className="basal">
       <h1>Calculadora de Taxa de Metabolismo Basal</h1>
       <div className="basal__calc">
-a
+        <InputBasalLeft />
+        <InputBasalLeft />
+        <InputBasalRight />
+        <InputBasalRight />
       </div>
-      <div className="basal__footer">
-b
-      </div>
+      <div className="basal__footer">b</div>
     </section>
-  )
-}
+  );
+};
 
-export default Basal
+const InputBasalLeft = () => {
+  return <input className="input-left active-left"></input>;
+};
+
+const InputBasalRight = () => {
+  return <input className="input-right active-right"></input>;
+};
+
+export default Basal;
