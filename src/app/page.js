1c7fa37ac53befc11f766a79a12f230be649cb91
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-02-20 23:34:16 -0300

    criada estrutura da calculadora basal

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 
+import Basal from '@/components/Basal/Basal'
+
 export default function Home() {
   return (
     <main>
-     Alou
+     <Basal />
     </main>
   )
 }
---
+++ src/components/Basal/Basal.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import "./Basal.scss"
+
+const Basal = () => {
+  return (
+    <section className='basal'>
+      <h1>Calculadora de Taxa de Metabolismo Basal</h1>
+      <div className="basal__calc">
+a
+      </div>
+      <div className="basal__footer">
+b
+      </div>
+    </section>
+  )
+}
+
+export default Basal
