a92a40263ec569b9898abb192902d714f77e6f42
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-02-21 13:21:17 -0300

    estrutura de inputs criada

--- src/components/Basal/Basal.jsx
+++ src/components/Basal/Basal.jsx
@@ -11,18 +11,28 @@ const Basal = () => {
         <InputBasalLeft />
         <InputBasalRight />
         <InputBasalRight />
+        <InputActivity />
+        <Calcular>Calcular</Calcular>
       </div>
       <div className="basal__footer">b</div>
     </section>
   );
 };
 
-const InputBasalLeft = () => {
+const InputBasalLeft = ({active}) => {
   return <input className="input-left active-left"></input>;
 };
 
-const InputBasalRight = () => {
+const InputBasalRight = ({active}) => {
   return <input className="input-right active-right"></input>;
 };
+
+const InputActivity = ({active}) => {
+  return <input className="input-activity" ></input>
+}
+
+const Calcular = ({active, children}) => {
+  return <button className="btn-calcular btn-active">{children}</button>
+}
 
 export default Basal;
